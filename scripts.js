3031451975f5313695c296977583448748996f9b
MahaAldawood9 <ma8279@nyu.edu>
2025-04-03 11:30:50 -0400

    preload images

--- scripts.js
+++ scripts.js
@@ -1,39 +1,56 @@
+function preloadImages() {
+    const imageSources = [
+        "images/MN.png",
+        "images/BK.png",
+        "images/QN.png",
+        "images/BX.png",
+        "images/SI.png",
+        "images/NYC.png"
+    ];
+    imageSources.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+}
+
+
 function injectImage(borough) {
     const img = document.createElement("img");
 
 
     switch (borough) {
-      case 'manhattan':
-        img.src = "https://i.imgur.com/GqXlU24.png"; 
-        img.alt = "Manhattan Map";
-        break;
-      case 'brooklyn':
-        img.src = "https://i.imgur.com/NvSz04m.png"; 
-        img.alt = "Brooklyn Map";
-        break;
-      case 'queens':
-        img.src = "https://i.imgur.com/yaOcOEO.png"; 
-        img.alt = "Queens Map";
-        break;
-      case 'thebronx':
-        img.src = "https://i.imgur.com/3Ke2OxT.png"; 
-        img.alt = "The Bronx Map";
-        break;
-      case 'statenisland':
-        img.src = "https://i.imgur.com/4G10gxH.png"; 
-        img.alt = "Staten Island Map";
-        break;
-      default:
+        case 'manhattan':
+            img.src = "images/MN.png";
+            img.alt = "Manhattan Map";
+            break;
+        case 'brooklyn':
+            img.src = "images/BK.png";
+            img.alt = "Brooklyn Map";
+            break;
+        case 'queens':
+            img.src = "images/QN.png";
+            img.alt = "Queens Map";
+            break;
+        case 'thebronx':
+            img.src = "images/BX.png";
+            img.alt = "The Bronx Map";
+            break;
+        case 'statenisland':
+            img.src = "images/SI.png";
+            img.alt = "Staten Island Map";
+            break;
 
-        img.src = "https://i.imgur.com/aqFYgjp.png"; 
-        img.alt = "NYC Map";
+        default:
+            img.src = "images/NYC.png";
+            img.alt = "NYC Map";
     }
-    
-        const container = document.getElementById("maps");
-    container.innerHTML = '';  
-    container.appendChild(img); 
-  }
-  
-    window.onload = function() {
+
+    const container = document.getElementById("maps");
+    container.innerHTML = '';
+    container.appendChild(img);
+}
+
+window.onload = function () {
+    preloadImages();
     injectImage('default');
-  }
+}
